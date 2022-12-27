af2ec05b353312d73c240a7f8edcf66df75a52a6
AnlSakar <anilsakar01@gmail.com>
2022-12-27 20:23:01 +0300

    27.12.2022

--- TraversalCoreProje/Controllers/DestinationController.cs
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -12,5 +12,18 @@ namespace TraversalCoreProje.Controllers
             var values = destinationManager.TGetList();
             return View(values);
         }
+
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            ViewBag.i = id;
+            var values = destinationManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(DestinationController p)
+        {
+            return View();
+        }
     }
 }
